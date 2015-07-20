a3fd036778340b66dbda953a203c3d3cae35f564
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-07-21 02:02:11 +0900

    とりあえず go-ircevent をラッピング

---
+++ ape.go
@@ -0,0 +1,23 @@
+package ape
+
+import "github.com/thoj/go-ircevent"
+
+type Connection struct {
+	*irc.Connection
+}
+
+type Event struct {
+	*irc.Event
+}
+
+func (con *Connection) AddCallback(eventCode string, callback func(*Event)) string {
+	return con.Connection.AddCallback(eventCode, func(e *irc.Event) {
+		callback(&Event{e})
+	})
+}
+
+func New(nickname, username string) *Connection {
+	return &Connection{
+		irc.IRC(nickname, username),
+	}
+}
